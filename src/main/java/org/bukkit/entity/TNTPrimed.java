a370fccae791f39d7842e131e0832e7040fa376d
Byteflux <byte@byteflux.net>
2016-02-29 17:50:31 -0600

    Entity Origin API

--- src/main/java/org/bukkit/entity/TNTPrimed.java
+++ src/main/java/org/bukkit/entity/TNTPrimed.java
@@ -39,4 +39,15 @@ public interface TNTPrimed extends Explosive {
      */
     @Nullable
     public Entity getSource();
+
+    /**
+     * Gets the source block location of the TNTPrimed
+     *
+     * @return the source block location the TNTPrimed was spawned from
+     * @deprecated replaced by {@link Entity#getOrigin()}
+     */
+    @Deprecated
+    default org.bukkit.Location getSourceLoc() {
+        return this.getOrigin();
+    }
 }
