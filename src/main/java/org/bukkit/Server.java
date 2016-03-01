a86c59179fdbbb39f2b798a4b5894ae97f1870c9
kashike <kashike@vq.lc>
2016-02-29 19:48:59 -0600

    Expose server CommandMap

--- src/main/java/org/bukkit/Server.java
+++ src/main/java/org/bukkit/Server.java
@@ -1132,6 +1132,15 @@ public interface Server extends PluginMessageRecipient {
     public double[] getTPS();
     // Paper end
 
+    // Paper start
+    /**
+     * Gets the active {@link org.bukkit.command.CommandMap}
+     *
+     * @return the active command map
+     */
+    @NotNull
+    org.bukkit.command.CommandMap getCommandMap();
+
     /**
      * Get the advancement specified by this key.
      *
