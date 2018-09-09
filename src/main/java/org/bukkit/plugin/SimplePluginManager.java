39caeaaad5de2bf25b560a5ec3f5ca666da7967f
Aikar <aikar@aikar.co>
2018-09-09 00:32:05 -0400

    Remove deadlock risk in firing async events
    The PluginManager incorrectly used synchronization on firing any event
    that was marked as synchronous.
    
    This synchronized did not even protect any concurrency risk as
    handlers were already thread safe in terms of mutations during event
    dispatch.
    
    The way it was used, has commonly led to deadlocks on the server,
    which results in a hard crash.
    
    This change removes the synchronize and adds some protection around enable/disable

--- src/main/java/org/bukkit/plugin/SimplePluginManager.java
+++ src/main/java/org/bukkit/plugin/SimplePluginManager.java
@@ -398,7 +398,7 @@ public final class SimplePluginManager implements PluginManager {
      * @return true if the plugin is enabled, otherwise false
      */
     @Override
-    public boolean isPluginEnabled(@Nullable Plugin plugin) {
+    public synchronized boolean isPluginEnabled(@Nullable Plugin plugin) { // Paper - synchronize
         if ((plugin != null) && (plugins.contains(plugin))) {
             return plugin.isEnabled();
         } else {
@@ -407,7 +407,7 @@ public final class SimplePluginManager implements PluginManager {
     }
 
     @Override
-    public void enablePlugin(@NotNull final Plugin plugin) {
+    public synchronized void enablePlugin(@NotNull final Plugin plugin) { // Paper - synchronize
         if (!plugin.isEnabled()) {
             List<Command> pluginCommands = PluginCommandYamlParser.parse(plugin);
 
@@ -445,7 +445,7 @@ public final class SimplePluginManager implements PluginManager {
     }
 
     @Override
-    public void disablePlugin(@NotNull final Plugin plugin, boolean closeClassloader) {
+    public synchronized void disablePlugin(@NotNull final Plugin plugin, boolean closeClassloader) { // Paper - synchronize
         // Paper end - close Classloader on disable
         if (plugin.isEnabled()) {
             try {
@@ -506,6 +506,7 @@ public final class SimplePluginManager implements PluginManager {
             defaultPerms.get(false).clear();
         }
     }
+    private void fireEvent(Event event) { callEvent(event); } // Paper - support old method incase plugin uses reflection
 
     /**
      * Calls an event with the given details.
@@ -516,25 +517,11 @@ public final class SimplePluginManager implements PluginManager {
      */
     @Override
     public void callEvent(@NotNull Event event) {
-        if (event.isAsynchronous()) {
-            if (Thread.holdsLock(this)) {
-                throw new IllegalStateException(event.getEventName() + " cannot be triggered asynchronously from inside synchronized code.");
-            }
-            if (server.isPrimaryThread()) {
-                throw new IllegalStateException(event.getEventName() + " cannot be triggered asynchronously from primary server thread.");
-            }
-            fireEvent(event);
-        } else {
-            if (!server.isPrimaryThread()) {
-                throw new IllegalStateException(event.getEventName() + " cannot be triggered asynchronously from another thread.");
-            }
-            synchronized (this) {
-                fireEvent(event);
-            }
+        // Paper - replace callEvent by merging to below method
+        if (event.isAsynchronous() && server.isPrimaryThread()) {
+            throw new IllegalStateException(event.getEventName() + " cannot be triggered asynchronously from primary server thread.");
         }
-    }
 
-    private void fireEvent(@NotNull Event event) {
         HandlerList handlers = event.getHandlers();
         RegisteredListener[] listeners = handlers.getRegisteredListeners();
 
