1161c0a0bc98e73d3ae83967105b34b542e7648c
Aikar <aikar@aikar.co>
2018-10-06 21:14:29 -0400

    Material API additions

--- src/main/java/org/bukkit/Material.java
+++ src/main/java/org/bukkit/Material.java
@@ -80,6 +80,7 @@ import org.jetbrains.annotations.Nullable;
 /**
  * An enum of all material IDs accepted by the official server and client
  */
+@SuppressWarnings({"DeprecatedIsStillUsed", "deprecation"}) // Paper
 public enum Material implements Keyed {
     //<editor-fold desc="Materials" defaultstate="collapsed">
     ACACIA_BOAT(27326, 1),
@@ -3290,6 +3291,22 @@ public enum Material implements Keyed {
             throw new AssertionError(ex);
         }
     }
+
+    // Paper start
+
+    /**
+     * @return If the type is either AIR, CAVE_AIR or VOID_AIR
+     */
+    public boolean isEmpty() {
+        switch (this) {
+            case AIR:
+            case CAVE_AIR:
+            case VOID_AIR:
+                return true;
+        }
+        return false;
+    }
+    // Paper end
 
     /**
      * Do not use for any reason.
