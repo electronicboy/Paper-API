PaperMC/Paper

RangedEntity API
Allows you to determine if an entity is capable of ranged attacks,
and to perform an attack.

paper-api/src/main/java/org/bukkit/entity/Witch.java
@@ -1,9 +1,11 @@
 package org.bukkit.entity;
 
+import com.destroystokyo.paper.entity.RangedEntity;
+
 /**
  * Represents a Witch
  */
-public interface Witch extends Raider {
+public interface Witch extends Raider, RangedEntity { // Paper
 
     /**
      * Gets whether the witch is drinking a potion

electronicboy/Paper-API

src/main/java/org/bukkit/entity/Witch.java
@@ -1,7 +1,9 @@
 package org.bukkit.entity;
 
+import com.destroystokyo.paper.entity.RangedEntity;
+
 /**
  * Represents a Witch
  */
-public interface Witch extends Monster {
+public interface Witch extends Monster, RangedEntity { // Paper
 }