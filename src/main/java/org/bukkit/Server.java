b298613927e3bcdd2400e87c98b53a9bcbc08178
Aikar <aikar@aikar.co>
2019-03-24 18:39:01 -0400

    Flip some Spigot API null annotations
    while some of these may of been true, they are extreme cases and cause
    a ton of noise to plugin developers.
    
    These do not help plugin developers if they bring moise noise than value.

--- src/main/java/org/bukkit/Server.java
+++ src/main/java/org/bukkit/Server.java
@@ -962,7 +962,7 @@ public interface Server extends PluginMessageRecipient {
      *
      * @return the scoreboard manager or null if no worlds are loaded.
      */
-    @Nullable
+    @NotNull // Paper
     ScoreboardManager getScoreboardManager();
 
     /**
@@ -1232,7 +1232,7 @@ public interface Server extends PluginMessageRecipient {
      * @param clazz the class of the tag entries
      * @return the tag or null
      */
-    @Nullable
+    @UndefinedNullability
     <T extends Keyed> Tag<T> getTag(@NotNull String registry, @NotNull NamespacedKey tag, @NotNull Class<T> clazz);
 
     /**
