PaperMC/Paper

Add MetadataStoreBase.removeAll(Plugin)
So that on reload, metadata will be cleared

paper-api/src/main/java/org/bukkit/metadata/MetadataStoreBase.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator; // Paper
 import java.util.List;
 import java.util.Map;
 import java.util.WeakHashMap;
@@ -126,6 +127,26 @@ public abstract class MetadataStoreBase<T> {
         for (Map<Plugin, MetadataValue> values : metadataMap.values()) {
             if (values.containsKey(owningPlugin)) {
                 values.get(owningPlugin).invalidate();
+            }
+        }
+    }
+
+    /**
+     * Removes all metadata in the metadata store that originates from the
+     * given plugin.
+     *
+     * @param owningPlugin the plugin requesting the invalidation.
+     * @throws IllegalArgumentException If plugin is null
+     */
+    public void removeAll(@NotNull Plugin owningPlugin) {
+        Preconditions.checkNotNull(owningPlugin, "Plugin cannot be null");
+        for (Iterator<Map<Plugin, MetadataValue>> iterator = metadataMap.values().iterator(); iterator.hasNext(); ) {
+            Map<Plugin, MetadataValue> values = iterator.next();
+            if (values.containsKey(owningPlugin)) {
+                values.remove(owningPlugin);
+            }
+            if (values.isEmpty()) {
+                iterator.remove();
             }
         }
     }

electronicboy/Paper-API

src/main/java/org/bukkit/metadata/MetadataStoreBase.java
@@ -127,6 +127,26 @@ public abstract class MetadataStoreBase<T> {
     }
 
     /**
+     * Removes all metadata in the metadata store that originates from the
+     * given plugin.
+     *
+     * @param owningPlugin the plugin requesting the invalidation.
+     * @throws IllegalArgumentException If plugin is null
+     */
+    public void removeAll(@NotNull Plugin owningPlugin) {
+        Validate.notNull(owningPlugin, "Plugin cannot be null");
+        for (Iterator<Map<Plugin, MetadataValue>> iterator = metadataMap.values().iterator(); iterator.hasNext(); ) {
+            Map<Plugin, MetadataValue> values = iterator.next();
+            if (values.containsKey(owningPlugin)) {
+                values.remove(owningPlugin);
+            }
+            if (values.isEmpty()) {
+                iterator.remove();
+            }
+        }
+    }
+
+    /**
      * Creates a unique name for the object receiving metadata by combining
      * unique data from the subject with a metadataKey.
      * <p>